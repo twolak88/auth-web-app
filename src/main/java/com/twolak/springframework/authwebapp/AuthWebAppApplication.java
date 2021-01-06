565aefcefcdcca506f062b7a285d5cdbf34ccb20
twolak88 <twolak88@gmail.com>
2021-01-06 21:29:14 +0100

    Initial project

---
+++ src/main/java/com/twolak/springframework/authwebapp/AuthWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.twolak.springframework.authwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/twolak/springframework/authwebapp/AuthWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twolak.springframework.authwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
